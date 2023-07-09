d7d76962b45660d0eee509bdc618080684177552
Aditya Singh Parihar <201037@kit.ac.in>
2023-07-09 21:19:38 +0530

    Responsive at 420px

---
+++ JS/javascript.js
@@ -0,0 +1,10 @@
+function opennavsection(){
+    document.querySelector(".menu-btn").style.display = "none"
+    document.querySelector(".btn-menu").style.right = "-15px"
+
+}
+function closenavsection(){
+    document.querySelector(".btn-menu").style.right = "-220px"
+    document.querySelector(".menu-btn").style.display = "block"
+
+}
